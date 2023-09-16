a88668c75d121efc5533c83f922e5a4f056cfd91
jclagoria <lagoria79@gmail.com>
2023-09-16 20:47:40 -0300

    add manage exceptions, add new router and handler

---
+++ src/main/java/ar/com/api/contracts/exception/external/CoinGeckoBadRequestException.java
@@ -0,0 +1,9 @@
+package ar.com.api.contracts.exception.external;
+
+public class CoinGeckoBadRequestException extends RuntimeException {
+ 
+ public CoinGeckoBadRequestException(String message) {
+  super(message);
+ }
+
+}
