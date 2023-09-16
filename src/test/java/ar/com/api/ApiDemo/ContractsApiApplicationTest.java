22138cb42c54b94713472f0c0f01fea6a619cb1c
jclagoria <lagoria79@gmail.com>
2023-09-16 16:41:19 -0300

    resolve error in test

--- src/test/java/ar/com/api/ApiDemo/ContractsApiApplicationTest.java
+++ src/test/java/ar/com/api/ApiDemo/ContractsApiApplicationTest.java
@@ -3,8 +3,8 @@ package ar.com.api.ApiDemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ApiDemoApplicationTests {
+@SpringBootTest(classes = ContractsApiApplicationTest.class)
+class ContractsApiApplicationTest {
 
 	@Test
 	void contextLoads() {
